4ea541f51563b3ab83693fa1b8c60617fdb4a3c5
“mbartlett413” <Mbartlett413@me.com>
2020-09-21 12:58:49 -0400

    working

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,8 @@ set :repo_url, "git@github.com:Mbartlett413/portfolioTwo.git"
 # Deploy to the user's home directory
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
+set :linked_files, %w{config/master.key}
+
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
 # Only keep the last 5 releases to save disk space
